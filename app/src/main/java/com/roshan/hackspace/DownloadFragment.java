d6c7642b4e1f4b881f2b4af9d105a37f9490f113
roshankr1998 <roshan.kumar.rk109@gamil.com>
2021-06-26 18:08:51 +0530

    initial commit2

--- app/src/main/java/com/roshan/hackspace/DownloadFragment.java
+++ app/src/main/java/com/roshan/hackspace/DownloadFragment.java
@@ -2,63 +2,35 @@ package com.roshan.hackspace;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DownloadFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class DownloadFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DownloadFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DownloadFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DownloadFragment newInstance(String param1, String param2) {
-        DownloadFragment fragment = new DownloadFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+ImageView returnimage;
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_download, container, false);
+        returnimage=root.findViewById(R.id.returnimage);
+        returnimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fr= getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fr.beginTransaction();
+                fragmentTransaction.replace(R.id.frame,new DashboardFragment()).commit();
+            }
+        });
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_download, container, false);
-    }
-}
+
+
+        return root;
+    }}
--- app/src/main/java/com/roshan/hackspace/UploadFragment.java
+++ app/src/main/java/com/roshan/hackspace/UploadFragment.java
@@ -2,63 +2,52 @@ package com.roshan.hackspace;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UploadFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class UploadFragment extends Fragment {
+TextView select;
+EditText pdfbook,pdfauth,pdfpubl;
+ImageButton selectfile;
+Button btnupload;
+ImageView returnimage;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UploadFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UploadFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UploadFragment newInstance(String param1, String param2) {
-        UploadFragment fragment = new UploadFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_upload, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload, container, false);
-    }
-}
+
+        select=root.findViewById(R.id.filename);
+        pdfbook=root.findViewById(R.id.file_name);
+        pdfauth=root.findViewById(R.id.file_author);
+        pdfpubl=root.findViewById(R.id.file_publication);
+        selectfile=root.findViewById(R.id.upload_imagebtn);
+        btnupload=root.findViewById(R.id.uploadbtn);
+        returnimage=root.findViewById(R.id.returnview);
+
+        returnimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fr= getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fr.beginTransaction();
+                fragmentTransaction.replace(R.id.frame,new DashboardFragment()).commit();
+            }
+        });
+
+        return root;
+    }}
