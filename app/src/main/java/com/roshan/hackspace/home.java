ac8cd29d110710cc2396996fdcf4172bd89f7cc2
roshankr1998 <roshan.kumar.rk109@gamil.com>
2021-06-28 13:13:28 +0530

    nav drawer not working on other phone name updated

--- app/src/main/java/com/roshan/hackspace/home.java
+++ app/src/main/java/com/roshan/hackspace/home.java
@@ -26,6 +26,11 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -40,6 +45,8 @@ public class home extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser user;
     String n,e,p,no;
+    FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+    DatabaseReference post=firebaseDatabase.getReference().child("Users");
 
 
 
@@ -209,7 +216,18 @@ public class home extends AppCompatActivity {
         NavigationView navigationView=(NavigationView)findViewById(R.id.nav_view);
         View headerview=navigationView.getHeaderView(0);
         TextView email=headerview.findViewById(R.id.email);
+        TextView name=headerview.findViewById(R.id.name);
         email.setText(user.getEmail());
+        post.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+
+                String username = snapshot.child("fullname").getValue(String.class);
+                name.setText(username);
+            }@Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), "Failed to load Profile ", Toast.LENGTH_SHORT).show();
+            }});
     }
 
 
