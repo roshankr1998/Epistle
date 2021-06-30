8a32cfe9ef4910f3b158017389135a455ce9461f
roshankr1998 <roshan.kumar.rk109@gamil.com>
2021-06-30 19:21:54 +0530

    splash also added

---
+++ app/src/main/java/com/roshan/hackspace/splash_screen.java
@@ -0,0 +1,50 @@
+package com.roshan.hackspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class splash_screen extends AppCompatActivity {
+
+    private static int Splash=5000;
+    Animation topanim,bottomanim;
+    ImageView imageView14;
+    TextView textView11,textView7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    topanim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+    bottomanim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+    imageView14=findViewById(R.id.imageView14);
+    textView7=findViewById(R.id.textView7);
+    textView11=findViewById(R.id.textView11);
+    imageView14.setAnimation(topanim);
+    textView7.setAnimation(bottomanim);
+    textView11.setAnimation(topanim);
+
+    new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            Intent intent=new Intent(splash_screen.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+    },Splash);
+
+
+
+
+
+
+    }
+}
